874c5afc3ad72b3451e03b1961943035e5d72e2e
Casey Manning <casey@playground.global>
2019-08-09 16:43:49 -0700

    successfully found packet

--- src/urx/Networker.java
+++ src/urx/Networker.java
@@ -10,7 +10,9 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.DoubleBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Networker {
 	
@@ -46,21 +48,64 @@ public class Networker {
 	
 	class Reciever extends Thread {
 		
+		private final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+		public String bytesToHex(Byte[] bytes) {
+		    char[] hexChars = new char[bytes.length * 2];
+		    for (int j = 0; j < bytes.length; j++) {
+		        int v = bytes[j] & 0xFF;
+		        hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+		        hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		    }
+		    return new String(hexChars);
+		}
+
+		
 		@Override
 		public void run() {
 			System.out.println("Starting Reciever Thread");
 			try {
-				byte[] recieved = socket.getInputStream().readNBytes(1386);
+//				byte[] received = socket.getInputStream().readNBytes(1386);
+				LinkedList<Byte> stored = new LinkedList<Byte>();
+				byte newByte = socket.getInputStream().readNBytes(1)[0];
+				stored.add(newByte);
+				newByte = socket.getInputStream().readNBytes(1)[0];
+				stored.add(newByte);
+				newByte = socket.getInputStream().readNBytes(1)[0];
+				stored.add(newByte);
+				newByte = socket.getInputStream().readNBytes(1)[0];
+				stored.add(newByte);
+				
 				while(true) {
-					recieved = socket.getInputStream().readNBytes(715);
-					ByteBuffer bb = ByteBuffer.wrap(recieved);
-
-					System.out.println("————————————————————————————————————");
-					Struct struct = new Struct();
-
-				    String ss = Arrays.toString(recieved);
-				    
-				    System.out.println(ss);
+					
+					Byte[] bytes = new Byte[stored.size()];
+					bytes = stored.toArray(bytes);
+					String hex = bytesToHex(bytes);
+					long packetSize = Long.parseLong(hex,16);
+					if(packetSize > 5 && packetSize < 2000) {
+						newByte = socket.getInputStream().readNBytes(1)[0];
+						Byte[] asdfa = new Byte[1];
+						asdfa[0] = newByte;
+						String hex2 = bytesToHex(asdfa);
+						int packetType = Integer.parseInt(hex2,16);
+						System.out.println("Type: " + packetType);
+						System.out.println("Hex: " + hex + hex2);
+						System.out.println("Size:" + packetSize);
+						if(packetType == 16) {
+							System.out.println("AAIUSHDIUASHIFASIUFHAIUSHFIUASHFIUASHFAUIHFASUHFIu");
+							byte[] restOfPacket = socket.getInputStream().readNBytes((int) packetSize);
+							System.out.println(Arrays.toString(restOfPacket));
+							Thread.sleep(1000);
+						}
+						
+						
+						
+					} else {
+						newByte = socket.getInputStream().readNBytes(1)[0];
+					}
+					
+					
+					stored.add(newByte);
+					stored.remove(0);
 
 				}
 			} catch (Exception e) {
